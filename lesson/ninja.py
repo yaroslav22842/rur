066729ef9be28c41ee2daf7eb7ebf212b3e38afd
yarik <Ayaroslav2011@gmail.com>
2022-11-11 10:16:17 +0200

    dz3

---
+++ lesson/ninja.py
@@ -0,0 +1,15 @@
+from charecter import Charecter
+import random
+
+class Ninja(Charecter):
+    def __init__(self, health, damage, defence, name):
+        Charecter.__init__(self, name, health, defence, damage)
+
+    def roll(self, heal):
+        dash = random.randint(1, 1000)
+        if dash <= 10:
+            self.heal_count()
+            self.health = self.health + heal
+
+
+
